5fe6767d0affa9901bfb0b5984f3b302e3d9cee1
Jonathan <jmoral4@gmail.com>
2014-12-06 15:15:37 -0500

    The Chase - Initial check-in
    Initial check-in of 'the chase' assets

---
+++ TheChase/Assets/The Chase/Scripts/Game Management/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject gameOverObject;
+	
+	private static GameManager instance;
+	
+	void Awake() {
+		instance = this;
+		if (gameOverObject != null) gameOverObject.SetActive(false);
+	}
+	
+	void InstanceGameOver() {
+		if (gameOverObject != null) gameOverObject.SetActive(true);
+		Time.timeScale = 0;
+	}
+	
+	public static void GameOver() {
+		instance.InstanceGameOver();
+	}
+	
+}
---
+++ TheChase/Assets/The Chase/Scripts/Game Management/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject source;
+	public int count = 10;
+	public float range = 30;
+		
+	void Start () {
+		if (source == null) return;
+		for (int i = 0; i < count; i++) {
+			var spawn = Instantiate(source) as GameObject;
+			var position = new Vector3(transform.position.x + Random.Range(-range, range),
+									   transform.position.y,
+									   transform.position.z + Random.Range(-range, range));
+			spawn.transform.position = position;
+			spawn.transform.SetParent(transform);
+		}
+	}
+	
+}
